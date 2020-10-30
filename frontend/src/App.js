f6dce331e68346b31e908718c5832fee53a4dc6a
ivanblinov2k17 <ivanblinov2015@mail.ru>
2020-10-30 23:58:27 +0300

    working styles for button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,18 +7,46 @@ import TeacherPage from './views/TeacherPage'
 import LoginForm from './views/LoginForm'
 // import NavigationPanel from './components/NavigationPanel'
 import AppBar from '@material-ui/core/AppBar';
-import {Toolbar} from '@material-ui/core/';
-
+import {Toolbar, Button, rgbToHex} from '@material-ui/core/';
+import {createMuiTheme} from '@material-ui/core/styles'
+import {ThemeProvider} from '@material-ui/core'
+const theme = createMuiTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiButton: {
+      root: {
+        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%) !important",
+        border: 0,
+        borderRadius: 3,
+        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+        color: "white",
+        height: 48,
+        padding: "0 30px",
+        margin: "0 10px",
+      },
+    },
+  },
+});
 function App() {
   return (
     <section className="App">
       <Router>        
         <AppBar position="static">
           <Toolbar>
-          <Link to="/about">Home</Link>        
-          <Link to="/teacher-page">Teacher page</Link>
-          <Link to="/student-page">Student Page</Link>
-          <Link to="/login">Login</Link>  
+            <ThemeProvider theme={theme}>
+            <Button>
+              <Link to="/about">Home</Link>        
+            </Button>
+            <Button>
+              <Link to="/teacher-page">Teacher page</Link>
+            </Button>
+            <Button>
+              <Link to="/student-page">Student Page</Link>
+            </Button>
+            <Button>
+              <Link to="/login">Login</Link>  
+            </Button>
+            </ThemeProvider> 
           </Toolbar>
         </AppBar>
           
