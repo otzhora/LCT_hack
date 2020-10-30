4f225b2be732027fcafec9398e6f391131f1c248
ivanblinov2k17 <ivanblinov2015@mail.ru>
2020-10-30 23:16:24 +0300

    working navigation panel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import { Link, BrowserRouter as Router, Route , Switch} from 'react-router-dom';
+import * as React from 'react'
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
 import IndexPage from './views/IndexPage'
 import StudentPage from './views/StudentPage'
 import TeacherPage from './views/TeacherPage'
 import LoginForm from './views/LoginForm'
+// import NavigationPanel from './components/NavigationPanel'
+import AppBar from '@material-ui/core/AppBar';
+import {Toolbar} from '@material-ui/core/';
+
 function App() {
   return (
     <section className="App">
       <Router>        
-       <Link to="/about">Home</Link>        
-       <Link to="/teacher-page">Teacher page</Link>
-       <Link to="/student-page">Student Page</Link>
-       <Link to="/login">Login</Link>    
+        <AppBar position="static">
+          <Toolbar>
+          <Link to="/about">Home</Link>        
+          <Link to="/teacher-page">Teacher page</Link>
+          <Link to="/student-page">Student Page</Link>
+          <Link to="/login">Login</Link>  
+          </Toolbar>
+        </AppBar>
+          
        
        <Route path="/about">
          <IndexPage></IndexPage>
