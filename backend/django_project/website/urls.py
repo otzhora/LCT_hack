5613fad55c642f50ad529f46ba644c3d1b575a43
otzhora <y_rogachev@inbox.ru>
2020-10-31 01:34:12 +0400

    boilerplate for task api

--- backend/django_project/website/urls.py
+++ backend/django_project/website/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('task/<task_name:task_name>/', views.task, name="task")
 ]
--- backend/django_project/website/views.py
+++ backend/django_project/website/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from code.code_runners import *
+from code.code_converters import *
+
 
 def home(request):
     if request.method == 'GET':
         return HttpResponse("lol")
+
+
+def task(request):
+    pass
