cabbc460c15e1522c1e7ffe23648f82fa7818cd7
ZharkovMihail <56646565m@mail.ru>
2020-10-31 01:05:35 +0300

    database + fix

--- backend/django_project/website/models.py
+++ backend/django_project/website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    task = models.TextField()
+    path = models.TextField()
+    teacher = models.ForeignKey('Teacher', related_name='tasks', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- backend/django_project/website/views.py
+++ backend/django_project/website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from code.code_runners import *
-from code.code_converters import *
+# from code.code_runners import *
+# from code.code_converters import *
 
 
 def home(request):
