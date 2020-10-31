4beca8acc53379a2afd415dee2134048adca250c
otzhora <y_rogachev@inbox.ru>
2020-10-31 04:43:06 +0400

    new task ep

--- backend/django_project/website/urls.py
+++ backend/django_project/website/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import ArticleView, TaskView, CodeView
+from .views import ArticleView, TaskView, CodeView, NewTaskView
 
 urlpatterns = [
     path('', ArticleView.as_view(), name='home'),
     path('task/<slug:url>/', TaskView.as_view(), name="task"),
-    path('code/<slug:username>/<slug:url>/', CodeView.as_view(), name="code")
+    path('code/<slug:username>/<slug:url>/', CodeView.as_view(), name="code"),
+    path('new_task', NewTaskView.as_view(), name="new_task")
 ]
