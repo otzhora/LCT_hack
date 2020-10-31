8d3d6153b222bfd0fb07c9f9a7f5e354555ef861
otzhora <y_rogachev@inbox.ru>
2020-10-31 05:40:12 +0400

    get results ep

--- backend/django_project/website/urls.py
+++ backend/django_project/website/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-from .views import ArticleView, TaskView, CodeView, NewTaskView, TasksView
+from .views import ArticleView, TaskView, CodeView, NewTaskView, TasksView, ResultView
 
 
 urlpatterns = [
@@ -10,6 +10,6 @@ urlpatterns = [
     path('task/<slug:url>/', TaskView.as_view(), name="task"),
     path('tasks/', TasksView.as_view(), name="tasks"),
     path('code/<slug:username>/<slug:url>/', CodeView.as_view(), name="code"),
-    path('new_task', NewTaskView.as_view(), name="new_task")
-
+    path('new_task', NewTaskView.as_view(), name="new_task"),
+    path('results/<slug:username>/<slug:url>/', ResultView.as_view(), name="result"),
 ]
