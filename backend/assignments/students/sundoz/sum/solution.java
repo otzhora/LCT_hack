713f526aee75d34f8a3fcb1fc9eaad4afc954f1b
sundoz <sundoz@mail.ru>
2020-10-31 20:38:02 +0300

    ruby

---
+++ backend/assignments/students/sundoz/sum/solution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class solution {
+   public static void main(String[] args) {
+      Scanner my_scan = new Scanner(System.in);
+      int a = my_scan.nextInt();
+      int b = my_scan.nextInt();
+      System.out.println(a + b);
+      
+   }
+}
