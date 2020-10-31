849bcdf95d1822e67adc825e62ed075f61f38bc9
ZharkovMihail <56646565m@mail.ru>
2020-10-31 04:56:42 +0300

    Class, codereview

--- backend/django_project/website/models.py
+++ backend/django_project/website/models.py
@@ -19,7 +19,16 @@ class Task(models.Model):
         return self.title
 
 
+class Class(models.Model):
+    name = models.CharField(max_length=120)
+    teacher = models.ForeignKey('Teacher', related_name='classes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
 class Student(models.Model):
+    Class = models.ForeignKey('Class', related_name="students", on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -30,4 +39,10 @@ class TaskResult(models.Model):
     task = models.ForeignKey('Task', related_name='taskResults', on_delete=models.CASCADE)
     student = models.ForeignKey('Student', related_name='taskResults', on_delete=models.CASCADE)
     mark = models.FloatField()
+
+
+class CodeReview(models.Model):
+    comment = models.TextField()
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    task = models.OneToOneField(Task, on_delete=models.CASCADE)
 
