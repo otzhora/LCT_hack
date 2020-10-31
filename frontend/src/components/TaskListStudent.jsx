cdf585118e8ad1adadb1937405f5ecbbafa174b8
ivanblinov2k17 <ivanblinov2015@mail.ru>
2020-10-31 22:01:00 +0300

    added soft url link to list button

--- frontend/src/components/TaskListStudent.jsx
+++ frontend/src/components/TaskListStudent.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+
 import { useState } from 'react';
 import { useEffect } from 'react';
 
@@ -22,28 +19,29 @@ const useStyles = makeStyles((theme) => ({
 function ListItemLink(props) {
   return <ListItem button component="a" {...props} />;
 }
-const tasks1 = [
-  {id: 1, name: 'First Task'},
-  {id: 2, name: 'Second Task'}  
-]
 
 export default function SimpleList() {
   const classes = useStyles();
-  const [tasks, setTasks] = useState({tasks:[]});
+  const [tasks, setTasks] = useState([]);
   async function loadTasks(){
     fetch(`http://localhost:8000/tasks/`,{
         method: 'GET',
       })
         .then(res => (res.ok ? res : Promise.reject(res)))
-        .then(res => res.json()).then(res=>{ setTasks(res); console.log(tasks,'sas')})
+        .then(res => res.json()).then(res=>{ setTasks(res.tasks); console.log(tasks,'sas')})
   }
   useEffect(()=>{loadTasks()},[])
   return (
     <div className={classes.root}>
       <List component="nav" aria-label="main mailbox folders">
-        {tasks.tasks.map(task=>(<ListItem button>
-          <ListItemText primary={task.title}  />
-        </ListItem>))}
+        {tasks.map(task=>{
+          const url = task.path.split('/').pop()
+          return (
+          <ListItemLink href={`#${url}`}>
+            <ListItemText primary={task.title}  />
+          </ListItemLink>)
+        }
+          )}
         
       </List>
       
