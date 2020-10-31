31b91a0150026993de6bfb8a8ea18cdec6e1b038
ZharkovMihail <56646565m@mail.ru>
2020-10-31 04:21:00 +0300

    admin bd

--- backend/django_project/website/admin.py
+++ backend/django_project/website/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Teacher, Task
+from .models import Teacher, Task, Student, TaskResult
 
 admin.site.register(Teacher)
 admin.site.register(Task)
+admin.site.register(TaskResult)
+admin.site.register(Student)
 
