a4d68ac8f698b7509f730d265015476d1998d794
ZharkovMihail <56646565m@mail.ru>
2020-10-31 03:49:44 +0300

    tasksview

--- backend/django_project/website/views.py
+++ backend/django_project/website/views.py
@@ -25,6 +25,13 @@ class ArticleView(APIView):
         return Response({"articles": "articles"})
 
 
+class TasksView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response({"tasks": serializer.data})
+
+
 class TaskView(APIView):
     def get(self, request, url):
         task = Task.objects.get(url=url)
